7b6998808a574a692b609b1870599bf1ec29ce1b
osmartin22 <oemartinez@ucdavis.edu>
2017-09-21 18:41:15 -0700

    First Commit. Notes App does not use permanent storage yet. Can add or delete notes

---
+++ app/src/main/java/com/ozmar/notes/NoteEditorActivity.java
@@ -0,0 +1,53 @@
+package com.ozmar.notes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    int noteID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+
+        editText = (EditText)findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        noteID = intent.getIntExtra("noteID", -1);
+        if(noteID != -1) {
+            editText.setText(MainActivity.notes.get(noteID));
+        }
+
+        else {
+            MainActivity.notes.add("");
+            noteID = MainActivity.notes.size() - 1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.notes.set(noteID, String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
